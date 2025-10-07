75d7e7201bc45f0ee88a0421fee9e477195b9cae
Aniksin8650 <aniketsingha8650@gmail.com>
2025-10-07 13:19:31 +0530

    Add files via upload

---
+++ Attachfile/src/main/java/com/example/attachfile/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.example.attachfile.controller;
+
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+
+import com.example.attachfile.entity.Employee;
+import com.example.attachfile.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/{empId}")
+    public Employee getEmployeeByEmpId(@PathVariable("empId") String empId) {
+        System.out.println("Fetching employee with empId: " + empId);
+        return employeeRepository.findByEmpId(empId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Employee not found for empId: " + empId ));
+    }
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
