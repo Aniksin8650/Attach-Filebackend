0055389833faafe0bbe16a64ce40ca3d9315f955
Aniksin8650 <aniketsingha8650@gmail.com>
2025-10-14 20:03:29 +0530

    Delete Attachfile directory

--- Attachfile/src/main/java/com/example/attachfile/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.attachfile.repository;
-
-import com.example.attachfile.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    Optional<Employee> findByEmpId(String empId);
-}
